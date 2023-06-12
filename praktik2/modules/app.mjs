2369a9cb7efa84b226b968fa986c9ecd10647545
Deluna170803 <devalubna08@gmail.com>
2023-06-12 08:56:36 +0700

    add ketinggalan

--- praktik2/modules/app.mjs
+++ praktik2/modules/app.mjs
@@ -1,1 +1,11 @@
+//panggil data pake import
 import { index, store } from "./controller.mjs";
+
+const main = function () {
+    const user = { name: " Deva", age: 20 }
+    store(user);
+    index();
+
+};
+
+main();
